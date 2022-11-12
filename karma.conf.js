c25a1292aea1219fa55af6f20e24dd13190a15ef
noydono <th.gabillard@laposte.net>
2022-11-12 16:58:09 +0100

    change name project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/untitled1'),
+      dir: require('path').join(__dirname, './coverage/findyourlostproto'),
       subdir: '.',
       reporters: [
         { type: 'html' },
